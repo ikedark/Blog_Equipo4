3cdb7ab962808baf6fa1b04bac3eafacffd95486
David <David@DESKTOP-5FKM5FI>
2021-06-11 18:19:05 -0700

    Agregue clases negocio y modifique algunas

--- BlogEquipo4/src/ObjetosNegocio/Municipio.java
+++ BlogEquipo4/src/ObjetosNegocio/Municipio.java
@@ -12,4 +12,58 @@ import org.bson.types.ObjectId;
  */
 public class Municipio {
     private ObjectId id;
+    private String nombre;
+
+    public Municipio(ObjectId id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Municipio other = (Municipio) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Municipio{" + "id=" + id + ", nombre=" + nombre + '}';
+    }
+    
+    
 }
